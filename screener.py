7ed3e84ba674f3e7ff1c8cf1326f50f55e6b41b4
Dhanabalzx <dhanapal.nba1@gmail.com>
2025-06-04 12:28:24 +0530

    Update screener.py

--- screener.py
+++ screener.py
@@ -1,6 +1,6 @@
 import requests
 
-BOT_TOKEN = 7853206716:AAGMWNSMeNyyS6OjZPujo8nMnqDjWCjo1LY
+BOT_TOKEN = "7853206716:AAGMWNSMeNyyS6OjZPujo8nMnqDjWCjo1LY"
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/getUpdates"
 response = requests.get(url)
